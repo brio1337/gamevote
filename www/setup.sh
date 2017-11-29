d017ada0f3d7fb38390416525c703644d861b2b9
Brian Oraas <brian.oraas@varsitytutors.com>
2017-11-29 02:26:40 -0800

    Setup now supports Mac, and generates certificates

--- www/setup.sh
+++ www/setup.sh
@@ -1,4 +1,15 @@
-sudo apt install npm nodejs-legacy
-sudo setcap 'cap_net_bind_service=+ep' $(readlink -f $(which node))
+set -e
+
+os_name=$(uname)
+
+if [[ $os_name = Linux ]]; then
+    sudo apt install npm nodejs-legacy
+    sudo setcap 'cap_net_bind_service=+ep' $(readlink -f $(which node))
+elif [[ $os_name = Darwin ]]; then
+    brew install node
+fi
 rm -rf node_modules
 npm install
+openssl req -x509 -newkey rsa:2048 -nodes \
+    -subj "/C=CA/ST=BC/L=Victoria/O=brio o/OU=brio ou/CN=brio.software" \
+    -out cert.pem -keyout key.pem
