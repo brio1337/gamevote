eef8d13953bff17aee5463850db64f53c1826e80
Brian Oraas <brian.oraas@varsitytutors.com>
2018-06-13 01:53:19 -0700

    Remove seeds, use games.dump file instead

--- db/setup.sh
+++ db/setup.sh
@@ -2,9 +2,11 @@ set -e
 
 os_name=$(uname)
 
+export PGPASSWORD=games
+
 if [[ $os_name = Linux ]]
 	which psql > /dev/null || sudo apt-get install postgresql
-	if PGPASSWORD=games psql -U games -d postgres -c "SELECT 1" > /dev/null 2>&1; then
+	if psql -U games -d postgres -c "SELECT 1" > /dev/null 2>&1; then
 		sudo sed -i -r -e 's/(local\s+all\s+all\s+)peer/\1md5/' /etc/postgresql/9.5/main/pg_hba.conf
 		sudo pg_ctlcluster 9.5 main reload
 	fi
@@ -14,12 +16,6 @@ elif [[ $os_name = Darwin ]]
 	pg_ctl -D pgdata start
 fi
 
-PGPASSWORD=games psql -U games -c "SELECT 1" > /dev/null 2>&1 || sudo -u postgres psql -v ON_ERROR_STOP=1 -f setupdb.sql
-PGPASSWORD=games psql -U games -v ON_ERROR_STOP=1 -f schema.sql
-PGPASSWORD=games psql -U games -v ON_ERROR_STOP=1 -f seed.sql
-
-player_files=`find seeds -type f -not -name gamelist.txt -exec basename {} .txt \;`
-for player in $players; do
-	player_file=`realpath seeds/$player.txt`
-	sudo -u postgres psql games -v ON_ERROR_STOP=1 -v player_file=$player_file -v player_name=$player -f load_votes.sql
-done
+psql -U games -c "SELECT 1" > /dev/null 2>&1 || sudo -u postgres psql -v ON_ERROR_STOP=1 -f setupdb.sql
+psql -U games -v ON_ERROR_STOP=1 -f schema.sql
+[ ! -f games.dump ] || psql -v ON_ERROR_STOP=1 -f games.dump
