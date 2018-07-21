d5f4dc95ad53409b38a364697706194d533fbba0
Brian Oraas <brian.oraas@varsitytutors.com>
2018-07-20 20:11:21 -0700

    Update with new migration

--- db/migrate.sql
+++ db/migrate.sql
@@ -1,11 +1,2 @@
-DROP TABLE IF EXISTS game_owners;
-CREATE TABLE game_owners (
-	game text,
-	player text,
-	PRIMARY KEY (game, player)
-);
-
-INSERT INTO game_owners
-SELECT game, unnest(owner) FROM games;
-
-ALTER TABLE games DROP COLUMN owner CASCADE;
+INSERT INTO players VALUES ('Consty');
+ALTER TABLE game_owners ADD FOREIGN KEY (game) REFERENCES games, ADD FOREIGN KEY (player) REFERENCES players;
