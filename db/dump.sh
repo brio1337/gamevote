7bd6ff45b5c68cf1fa43a31b4438d628da991463
Brian Oraas <brian.oraas@varsitytutors.com>
2020-05-06 23:49:28 -0700

    Clean db tools

--- db/dump.sh
+++ db/dump.sh
@@ -1,1 +1,2 @@
-pg_dump -h gamevote.cvsbgivadrlp.us-west-2.rds.amazonaws.com -U games -n public -x > games.dump
+source env/prod
+pg_dump -x > games.dump
--- db/restore-dump.sh
+++ db/restore-dump.sh
@@ -4,9 +4,11 @@ source env/local
 
 PSQL="psql -v ON_ERROR_STOP=1"
 
-dropdb --if-exists games
-createdb games
+# dropdb --if-exists games
+createdb games 2>/dev/null || true
+$PSQL -c "DROP SCHEMA public CASCADE"
+$PSQL -c "CREATE SCHEMA public"
+[ -e games.dump ] || ./dump.sh
 $PSQL -f games.dump
-#pg_restore -d games games.dump
 migration_list=(migrations/*)
 $PSQL ${migration_list[@]/#/-f }
