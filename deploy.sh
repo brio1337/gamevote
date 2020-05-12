0ed2e7dd9c0266e4212c80b77fa4d84555175fb8
Brian Oraas <brian.oraas@varsitytutors.com>
2020-05-11 22:19:46 -0700

    Remove unneeded configs

--- deploy.sh
+++
@@ -1,9 +0,0 @@
-set -e
-git push --force ssh://ubuntu@brio.software:/home/ubuntu/gamevote master
-ssh ubuntu@brio.software <<EOF
-set -e
-cd gamevote
-git reset --hard
-sudo systemctl restart node
-psql -v ON_ERROR_STOP=1 -U games -f db/views.sql
-EOF
